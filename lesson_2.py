47a4e4dc4337bf7682b63cf74f21d43013a3a989
Chiffilin <chiffilin@gmail.com>
2024-09-19 23:10:14 +0300

    #HW 2.1. Виведення числа в стовпчик #HW 2.2. Необхідно "перевернути" 5-ти значне число
    # first and second ver

--- lesson_2.py
+++ lesson_2.py
@@ -1,8 +1,10 @@
 #HW 2.1. Виведення числа в стовпчик
-#first ver
 user_input = int(input("Enter your number:"))
 user_input_v2 = user_input
 
+print("#HW 2.1. Виведення числа в стовпчик")
+
+#first ver
 print(user_input // 1000)
 user_input %= 1000
 print(user_input // 100)
@@ -18,3 +20,35 @@ print(divmod(user_input_v2%100,10))
 print(divmod(user_input_v2%10,1))
 
 
+# #HW 2.2. Необхідно "перевернути" 5-ти значне число
+user_input = int(input("Enter your number:"))
+user_input_v2 = user_input
+
+print(""""#HW 2.2. Необхідно "перевернути" 5-ти значне число""")
+
+#first ver
+number_1 = user_input % 10
+number_2 = (user_input // 10) % 10
+number_3 = (user_input // 100) % 10
+number_4 = (user_input // 1000) % 10
+number_5 = (user_input // 10000) % 10
+
+print(f'{number_1}{number_2}{number_3}{number_4}{number_5}')
+
+# second ver
+print(user_input % 10)
+user_input //= 10
+print(user_input % 10)
+user_input //= 10
+print(user_input % 10)
+user_input //= 10
+print(user_input % 10)
+user_input //= 10
+print(user_input % 10)
+
+# second ver
+print(divmod(user_input_v2 % 10,1))
+print(divmod(user_input_v2 % 100,10))
+print(divmod(user_input_v2 % 1000,100))
+print(divmod(user_input_v2 % 10000,1000))
+print(divmod(user_input_v2 % 100000,10000))
